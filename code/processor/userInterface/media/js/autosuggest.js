e912231f8ae6b30eb412e496998acbaa9bb8ad87
alagappanr <ralagappan2006@gmail.com>
2013-12-01 20:56:59 -0500

    Cleaned up all JS code
    Seperated JS code into files and including them
    in the base file

---
+++ code/processor/userInterface/media/js/autosuggest.js
@@ -0,0 +1,20 @@
+$('input#id_noun').typeahead({
+  name: 'noun',
+//  local: ['timtrueman', 'JakeHarding', 'vskarich']
+  //valueKey: "a.spellcheck.suggestions[1].suggestion",
+  remote: {
+    //url: "http://localhost:8983/solr/QACollection/suggest?q=name%3A%QUERY&wt=json&indent=true&json.wrf=callback",
+    url: "/suggest/q=%QUERY",
+    dataType: "json",
+    filter: function(data){
+      //alert("Filtering results");
+      var new_data = JSON.parse(data);
+      //alert(new_data);
+      var resultList = new_data.spellcheck.suggestions[1].suggestion;
+      //alert(resultList);
+      return resultList;
+    }
+  }
+  
+});
+
---
+++ code/processor/userInterface/media/js/tabchange.js
@@ -0,0 +1,6 @@
+$('.topic').click(function (e) {
+  //e.preventDefault();
+  //var id = $(this).attr('id');
+  var id = this.id;
+  alert(id);
+});
