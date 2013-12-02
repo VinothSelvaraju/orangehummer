bd44db6ed7c4d937cf8a95f792f61397a9f50e8b
alagappanr <ralagappan2006@gmail.com>
2013-12-01 20:13:30 -0500

    Cleaned up JavaScript code
    Cleaned up JS code for dropdown selection

--- code/processor/userInterface/media/js/dropdown-options.js
+++ code/processor/userInterface/media/js/dropdown-options.js
@@ -1,27 +1,3 @@
-$(document).ready(function(){
-  alert($('#id_col2').val());
-
-  $('#id_col2').empty();
-  $('#id_last_col').empty();
-var this_col2_options = {'Please select in order': ''};
-    $.each(this_col2_options, function(key, value) {
-      $('#id_col2').append($("<option></option>")
-           .attr("value", value).text(key));
-           });
-      $('#id_col2').val('');
-     var last_col_options = {'Please select in order':''};
-     $.each(last_col_options, function(key, value) {
-       $('#id_last_col').append($('<option></option>')
-           .attr('value', value).text(key));
-        });
-      $('#id_last_col').val('');
-});
-$('#id_fiveW').change(function(e){
-  $('#id_col2').empty();
-  $('#id_last_col').empty();
-  var fiveW_val = $('#id_fiveW').val();
-  //alert(fiveW_val);
-
   var col2_options ={'who':  {"Select--": "",
                             "is": "is",
                             "are": "are",
@@ -43,26 +19,8 @@ $('#id_fiveW').change(function(e){
                              },
                     '':{'Please select in order': ''}
                     };
-    var this_col2_options = col2_options[fiveW_val];
-    $.each(this_col2_options, function(key, value) {
-      $('#id_col2').append($("<option></option>")
-           .attr("value", value).text(key));
-           });
-      $('#id_col2').val('');
-     var last_col_options = {'Please select in order':''}
-     $.each(last_col_options, function(key, value) {
-       $('#id_last_col').append($('<option></option>')
-           .attr('value', value).text(key));
-        });
-      $('#id_last_col').val('');
-});
-$('#id_col2').change(function(e){
-  $('#id_last_col').empty();
-  var col2_val = $('#id_col2').val();
-  var fiveW_val = $('#id_fiveW').val();
-  //alert(fiveW_val);
 
-  var last_col_options ={'who':  {'': {'Please select in order': ''},
+   var last_col_options ={'who':  {'': {'Please select in order': ''},
                                   'is': {'spouse': 'spouse',
                                          'partner': 'partner',
                                          'employer': 'employer',
@@ -120,12 +78,4 @@ $('#id_col2').change(function(e){
                                              }
                                      }
                             };
-    var this_col_options = last_col_options[fiveW_val][col2_val];
-    $.each(this_col_options, function(key, value) {
-      $('#id_last_col').append($("<option></option>")
-           .attr("value", value).text(key));
-           });
-      $('#id_last_col').val('');
-});
-
-
+ 
---
+++ code/processor/userInterface/media/js/dropdown-selection.js
@@ -0,0 +1,72 @@
+//Works under the assumption that dropdown-options.js is called before this file
+$(document).ready(function(){
+  var fiveW_onload =$('#id_fiveW').val();
+  var col2_onload =$('#id_col2').val();
+  var last_col_onload =$('#id_last_col').val();
+
+  $('#id_col2').empty();
+  $('#id_last_col').empty();
+  if(fiveW_onload==''){ 
+      var this_col2_options = {'Please select in order': ''};
+        $.each(this_col2_options, function(key, value) {
+          $('#id_col2').append($("<option></option>")
+               .attr("value", value).text(key));
+               });
+          $('#id_col2').val('');
+         var this_col_options = {'Please select in order':''};
+         $.each(this_col_options, function(key, value) {
+           $('#id_last_col').append($('<option></option>')
+               .attr('value', value).text(key));
+            });
+          $('#id_last_col').val('');
+    }
+    else {
+       var this_col2_options = col2_options[fiveW_onload];
+        $.each(this_col2_options, function(key, value) {
+          $('#id_col2').append($("<option></option>")
+               .attr("value", value).text(key));
+               });
+          $('#id_col2').val(col2_onload);
+         var this_col_options = last_col_options[fiveW_onload][col2_onload];
+         $.each(this_col_options, function(key, value) {
+           $('#id_last_col').append($('<option></option>')
+               .attr('value', value).text(key));
+            });
+          $('#id_last_col').val(last_col_onload);
+
+    }
+});
+$('#id_fiveW').change(function(e){
+  $('#id_col2').empty();
+  $('#id_last_col').empty();
+  var fiveW_val = $('#id_fiveW').val();
+  //alert(fiveW_val);
+
+    var this_col2_options = col2_options[fiveW_val];
+    $.each(this_col2_options, function(key, value) {
+      $('#id_col2').append($("<option></option>")
+           .attr("value", value).text(key));
+           });
+      $('#id_col2').val('');
+     var this_col_options = {'Please select in order':''}
+     $.each(this_col_options, function(key, value) {
+       $('#id_last_col').append($('<option></option>')
+           .attr('value', value).text(key));
+        });
+      $('#id_last_col').val('');
+});
+$('#id_col2').change(function(e){
+  $('#id_last_col').empty();
+  var col2_val = $('#id_col2').val();
+  var fiveW_val = $('#id_fiveW').val();
+  //alert(fiveW_val);
+
+    var this_col_options = last_col_options[fiveW_val][col2_val];
+    $.each(this_col_options, function(key, value) {
+      $('#id_last_col').append($("<option></option>")
+           .attr("value", value).text(key));
+           });
+      $('#id_last_col').val('');
+});
+
+
